60b14757cc8b4ee5b4c0383593672035c4014b7a
isx46493081 <isx46493081@j22.informatica.escoladeltreball.org>
2021-02-10 13:02:56 +0100

    07bis

---
+++ 07bis-esdirectori.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+# @edt Gerard Pujol ASIX-M01
+# Febrer 2021
+# llistar directori si existeix
+#- validar rep un argument , validar que es directori, si es el llistem , si no error.
+#  $ prog dir
+# --------------------------------------------------------------
+ERR_NARGS=1
+ERR_NODIR=2
+# 1) Validem arguments
+if [ $# -ne 1 ]
+then
+        echo "Error: #arguments incorrectes"
+        echo "Usage: $0 dir"
+        exit $ERR_NARGS
+fi
+
+# 2) si té -h
+if [ $1 = "-h" ]
+then
+	echo "Help de l'ordre 07-esdirectori"
+	echo "@ edt ASX-M01 Curs 2021"
+	exit 0
+fi
+
+# 3) Saber si es directori
+mydir=$1
+if ! [ -d $mydir ]; 
+then
+  echo "Error: $mydir no és un directori"
+  echo "Usage: $0 dir"
+  exit $ERR_NODIR
+fi
+
+# Xixa
+ls $mydir
+exit 0
