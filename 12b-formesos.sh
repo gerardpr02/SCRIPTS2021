aead009e65c16be25e30bc0e2d48363c1b381d33
isx46493081 <isx46493081@j22.informatica.escoladeltreball.org>
2021-02-22 12:13:51 +0100

    12b

--- 12b-formesos.sh
+++ 12b-formesos.sh
@@ -9,36 +9,36 @@
 # 1) validar arguments
 # 2) iterar per cada element --> 2a) Validar que mes pren valor valid [1-12] 2b) determinar valor
 #------------------------------------------------------------------
+# CONSTANTS
 ERR_NARGS=1
-ERR_VALOR=2
 OK=0
-# 1) Validem arguments
-if [ $# -lt 1 ]
+
+# 1) Validem que hi ha un argument:
+if [ $# -ne 1 ]
 then
-        echo "Error: #arguments incorrectes"
-        echo "Usage: $0 mes[...]"
-        exit $ERR_NARGS
+  echo "ERROR: nº arguments incorrecte"
+  echo "Usage: $0 mes"
+  exit $ERR_NARGS
 fi
 
-
-
-error=1
-# bucle for
-for mes in $*
+# 2) Xixa:
+llista_mes=$1
+for mes in $llista_mes
 do
-	# 2a) Validar mes pren valor valid [0,10]
-	if ! [ $mes -ge 1 -a $mes -le 12 ]
-	then
-		echo "Error: Valor del mes $mes no vàlid [1-12]" >> /dev/stderr
-	# 2b) determinar valor
-	case $mes in 
-	1|3|5|7|9|11)
-		echo "El mes $mes té 31 dies" ;;
-	4|6|8|10)
-		echo "El mes $mes té 30 dies" ;;
-	2) 
-		echo "El mes $mes té 28 dies" ;;
-	*)
-		echo "Una altre cosa" ;;
+  if ! [ $mes -ge 1 -a $mes -le 12 ]
+  then
+     echo "ERROR: Valor de l'argument no vàlid." >> /dev/stderr
+     echo "Usage: $0 mes (valor de l'1 al 12)." >> /dev/stderr
+  fi
+  case $mes in
+      "2")
+        dies=28;;
+      "4"|"6"|"9"|"11")
+        dies=30;;
+      *)
+        dies=31;;
+  esac
 done
+
+echo "El mes: $mes, te $dies díes."
 exit $OK
