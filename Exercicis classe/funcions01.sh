44febc109614cf3ba823eb04afbd34364166ed91
isx46493081 <isx46493081@j22.informatica.escoladeltreball.org>
2021-04-07 12:59:37 +0200

    a

--- Exercicis classe/funcions01.sh
+++ Exercicis classe/funcions01.sh
@@ -2,6 +2,57 @@
 # Exemples de funcions
 
 
+
+function creaEscola(){
+  for classe in $*
+  do
+    creaClasse $classe
+  done
+
+
+
+}
+
+
+function creaClasse(){
+  classe=$1
+  PASSWD="alum"
+  llista_noms=$(echo ${classe}{01..02})
+  for user in $llista_noms
+  do
+    useradd $user
+    #echo "alum" | passwd --stdin $user &> /dev/null
+    #chpasswd < file
+    echo "$user:$PASSWD" | chpasswd 
+  done
+
+ 
+}
+
+
+
+
+
+
+function showAllGroups(){
+  MIN_USERS=2
+  llista_gids=$(cut -d: -f4 /etc/passwd | sort -n | uniq)
+  for gid in $llista_gids
+  do
+    count=$(egrep  "^[^:]*:[^:]*:[^:]*:$gid:" /etc/passwd | wc -l)	
+    gname=$(egrep "^[^:]*:[^:]*:$gid:" /etc/group | cut -d: -f1)
+    if [ $count -ge $MIN_USERS ]
+    then
+    gname=$(egrep "^[^:]*:[^:]*:$gid:" /etc/group | cut -d: -f1) 
+    echo "$gname($gid):$count"
+    egrep  "^[^:]*:[^:]*:[^:]*:$gid:" /etc/passwd | sort | cut -d: -f1,3,7 | sed -r 's/^(.*)$/\t\1/'
+    fi 
+
+  done
+  return 0
+}
+
+
 #Validar rep 1 arg
 #Validar existeix usuari
 # mostrar camp a camp
